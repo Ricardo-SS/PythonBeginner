c0ebd4d0d5f0a2d18810cf605550d436640f2c40
Ricardo-SS <ricardo.ufersa.pdf@gmail.com>
2022-10-20 16:14:05 -0300

    correção de erros

--- pythonExercicios/ex001.py
+++ pythonExercicios/ex001.py
@@ -1,8 +1,7 @@
+# criar um programa que print na tela uma mensagem.
 
+# usando uma variavel.
 mensagem = 'Ola, mundo'
 print(mensagem)
-
-# ou pode simplemente clocar o print
-print("ola, mundo.")
-
-
+# ou pode simplemente clocar o print, sem usar variavel.
+# print("ola, mundo.")
